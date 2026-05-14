77e8b85c0b6fe986909aa23ccecda8e7a77852a6
Nicolás Rodríguez Chaves <nicolasrodriguezch@hotmail.com>
2026-05-13 22:16:10 -0500

    removes import react

--- src/pages/registerVehicle/index.tsx
+++ src/pages/registerVehicle/index.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const RegisterVehicle = () => {
   return <div>RegisterVehicle</div>;
 };
--- src/pages/vehicleDetail/index.tsx
+++ src/pages/vehicleDetail/index.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const VehicleDetail = () => {
   return <div>VehicleDetail</div>;
 };
