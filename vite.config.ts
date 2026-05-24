b8af520d62e8bc82617aac611487f92c1c2f96b2
Nicolás Rodríguez Chaves <nicolasrodriguezch@hotmail.com>
2026-05-24 14:12:13 -0500

    handles multiple env vars

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,22 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: { '@': resolve(__dirname, 'src') },
-  },
-  server: {
-    proxy: {
-      '/ms-vehicles': 'http://localhost:8762',
-      '/ms-operations': 'http://localhost:8762',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_');
+  const apiUrl = env.VITE_API_URL || 'http://localhost:8762';
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: { '@': resolve(__dirname, 'src') },
     },
-  },
+    server: {
+      proxy: {
+        '/ms-vehicles': apiUrl,
+        '/ms-operations': apiUrl,
+      },
+    },
+  };
 });
