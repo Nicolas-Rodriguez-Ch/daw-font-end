35c33bd8c038f9022f2c495c416ec2f567efad15
Nicolás Rodríguez Chaves <nicolasrodriguezch@hotmail.com>
2026-05-24 17:47:59 -0500

    updates client code to handle multiple env vars

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
+const apiUrl = import.meta.env.PROD ? import.meta.env.VITE_API_URL : '';
+
 export const client = axios.create({
+  baseURL: apiUrl,
   headers: {
     'Content-Type': 'application/json',
   },
