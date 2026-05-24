71369726e45ff33eb7031cd37181eb46ee190da6
Nicolás Rodríguez Chaves <nicolasrodriguezch@hotmail.com>
2026-05-24 13:32:27 -0500

    updates register and vehicles pages to use api

---
+++ src/pages/registerVehicle/types.ts
@@ -0,0 +1,5 @@
+export type FormValues = {
+  vehicleModel: '';
+  vehicleBrand: '';
+  vehicleAvailability: '';
+};
--- src/pages/vehicles/index.tsx
+++ src/pages/vehicles/index.tsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react';
+
 import { Link } from 'react-router';
 
-import { mockData } from '@/assets';
 import { Card, TextCustom } from '@/components';
+import { vehicleService } from '@/services/vehicleService.ts';
+
+import type { Vehicle } from '../../../public';
 
 const Vehicles = () => {
+  const [vehicleData, setVehicleData] = useState<Vehicle[]>();
+
+  const handleFetchVehicles = async () => {
+    const { data } = await vehicleService.getAll();
+    return data;
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await handleFetchVehicles();
+      setVehicleData(data);
+    };
+
+    fetchData();
+  }, []);
   return (
     <>
       <div className='text-center mt-4'>
@@ -14,20 +33,32 @@ const Vehicles = () => {
         />
       </div>
       <div className='flex flex-col sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 p-4 sm:gap-4 sm:p-6 lg:p-8 w-full max-w-7xl mx-auto'>
-        {mockData.map((vehicle) => {
-          return (
-            <Link to={`/vehicles/${vehicle.id}`} key={vehicle.id}>
-              <Card
-                vehicle={vehicle}
-                primaryButton={{
-                  text: 'Vehicle Details',
-                  variant: 'muted',
-                  callback: () => {},
-                }}
-              />
+        {vehicleData ? (
+          vehicleData.map((vehicle) => {
+            return (
+              <Link to={`/vehicles/${vehicle.id}`} key={vehicle.id}>
+                <Card
+                  vehicle={vehicle}
+                  primaryButton={{
+                    text: 'Vehicle Details',
+                    variant: 'muted',
+                    callback: () => {},
+                  }}
+                />
+              </Link>
+            );
+          })
+        ) : (
+          <>
+            <TextCustom
+              text='No Vehicles in the system at this moment, try again later'
+              variant='heading'
+            />
+            <Link to={'register'}>
+              <TextCustom text='Register the first vehicle!' variant='regular' />
             </Link>
-          );
-        })}
+          </>
+        )}
       </div>
     </>
   );
